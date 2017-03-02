883810ea66e55bf6af7f5ad25f2fb83672170b74
Dennis van der Stelt <dvdstelt@outlook.com>
2017-03-02 10:07:40 +0100

    Added JetBrains dataannotations and guard class

---
+++ src/NServiceBus.WindowsPerformanceCounters/Utils/Guard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace NServiceBus
+{
+    static class Guard
+    {
+        [ContractAnnotation("value: null => halt")]
+        public static void AgainstNull([InvokerParameterName] string argumentName, [NotNull] object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void AgainstNegativeAndZero([InvokerParameterName] string argumentName, int value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+        }
+    }
+}
