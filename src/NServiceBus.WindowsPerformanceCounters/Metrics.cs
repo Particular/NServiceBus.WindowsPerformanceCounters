30c2800102d2f9281cdaab69cc6b896c418d0497
Simon Cropp <simon.cropp@gmail.com>
2017-03-14 09:54:01 +1100

    Metrics constructor should not be public

--- src/NServiceBus.WindowsPerformanceCounters/Metrics.cs
+++ src/NServiceBus.WindowsPerformanceCounters/Metrics.cs
@@ -6,14 +6,14 @@
 
     public static class MetricsExtensions
     {
-        public static Metrics Metrics(this EndpointConfiguration config)
+        public static Metrics Metrics(this EndpointConfiguration endpointConfiguration)
         {
-            Guard.AgainstNull(nameof(config), config);
+            Guard.AgainstNull(nameof(endpointConfiguration), endpointConfiguration);
 
             // This is bogus, but perhaps usable to verify it we access the extensionmethod via Metrics()
-            config.GetSettings().Set("Metrics", "Activated");
+            endpointConfiguration.GetSettings().Set("Metrics", "Activated");
 
-            return new Metrics(config);
+            return new Metrics(endpointConfiguration);
         }
     }
 
@@ -21,7 +21,7 @@
     {
         readonly EndpointConfiguration config;
 
-        public Metrics(EndpointConfiguration config) : base(config.GetSettings())
+        internal Metrics(EndpointConfiguration config) : base(config.GetSettings())
         {
             this.config = config;
         }
