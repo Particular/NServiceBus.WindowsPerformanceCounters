8a5adce043483ab8474e4f2ce3342bb72d480854
Simon Cropp <simon.cropp@gmail.com>
2017-03-14 09:47:09 +1100

    add missing null check

--- src/NServiceBus.WindowsPerformanceCounters/PerformanceCounters.cs
+++ src/NServiceBus.WindowsPerformanceCounters/PerformanceCounters.cs
@@ -14,6 +14,7 @@
 
         public PerformanceCounters EnableSLACounters(Action<PerformanceSettings> customizations)
         {
+            Guard.AgainstNull(nameof(customizations), customizations);
             customizations(new PerformanceSettings(settings));
             return this;
         }
