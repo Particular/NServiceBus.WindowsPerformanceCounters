570cb92d8ef63d07459e103dd08c4066e6d585ee
Dennis van der Stelt <dvdstelt@outlook.com>
2017-03-15 13:44:09 +0100

    Added comments to EnableWindowsPerformanceCounters

--- src/NServiceBus.WindowsPerformanceCounters/EndpointConfigurationExensionsForPerformanceCounters.cs
+++ src/NServiceBus.WindowsPerformanceCounters/EndpointConfigurationExensionsForPerformanceCounters.cs
@@ -4,6 +4,11 @@ namespace NServiceBus
 
     public static class EndpointConfigurationExensionsForPerformanceCounters
     {
+        /// <summary>
+        /// Enables statistical and criticaltime performance counters for this NServiceBus endpoint
+        /// </summary>
+        /// <param name="endpointConfiguration">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        /// <returns></returns>
         public static PerformanceCounters EnableWindowsPerformanceCounters(this EndpointConfiguration endpointConfiguration)
         {
             Guard.AgainstNull(nameof(endpointConfiguration), endpointConfiguration);
