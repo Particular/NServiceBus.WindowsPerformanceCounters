ad1d0072350fc22b0fd9548c0707410f61e00201
Simon Cropp <simon.cropp@gmail.com>
2017-03-17 13:26:29 +1100

    clean up test app

---
+++ src/TestApp/MyHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class MyHandler : IHandleMessages<MyCommand>
+{
+    public static Random random = new Random();
+
+    public Task Handle(MyCommand message, IMessageHandlerContext context)
+    {
+        var waitTime = random.Next(250);
+
+        Console.WriteLine($"Received message at {DateTime.Now}, delaying for {waitTime}ms");
+
+        return Task.Delay(waitTime);
+    }
+}
--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -1,58 +1,35 @@
 ﻿using System;
 using System.Threading.Tasks;
+using NServiceBus;
 
-namespace TestApp
+class Program
 {
-    using NServiceBus;
-    
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-        public async Task MainAsync()
-        {
-            var endpointConfig = new EndpointConfiguration("TestEndpoint");
-            endpointConfig.UsePersistence<InMemoryPersistence>();
-            endpointConfig.SendFailedMessagesTo("error");
-            endpointConfig.EnableInstallers();
-
-            endpointConfig.EnableWindowsPerformanceCounters()
-                .EnableSLAPerformanceCounters(TimeSpan.FromSeconds(10));
-
-            var endpoint = await Endpoint.Start(endpointConfig);
-
-            Console.WriteLine("Press a key to start sending messages...");
-            Console.ReadKey();
-
-            var rnd = new Random();
-            while (true)
-            {
-                var cmd = new MyCommand();
-                await endpoint.SendLocal(cmd);
-                await Task.Delay(rnd.Next(250));
-            }
-        }
+        new Program().MainAsync().GetAwaiter().GetResult();
     }
 
-    public class MyCommand : ICommand
+    async Task MainAsync()
     {
-        
-    }
-    
-    public class MyHandler : IHandleMessages<MyCommand>
-    {
-        public static Random random = new Random();
+        var endpointConfig = new EndpointConfiguration("TestEndpoint");
+        endpointConfig.UsePersistence<InMemoryPersistence>();
+        endpointConfig.SendFailedMessagesTo("error");
+        endpointConfig.EnableInstallers();
 
-        public async Task Handle(MyCommand message, IMessageHandlerContext context)
+        var performanceCounters = endpointConfig.EnableWindowsPerformanceCounters();
+        performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(10));
+
+        var endpoint = await Endpoint.Start(endpointConfig).ConfigureAwait(false);
+
+        Console.WriteLine("Press a key to start sending messages...");
+        Console.ReadKey();
+
+        var rnd = new Random();
+        while (true)
         {
-            var waitTime = random.Next(250);
-
-            Console.WriteLine($"Received message at {DateTime.Now}, delaying for {waitTime}ms");
-
-            await Task.Delay(waitTime);
+            var cmd = new MyCommand();
+            await endpoint.SendLocal(cmd).ConfigureAwait(false);
+            await Task.Delay(rnd.Next(250)).ConfigureAwait(false);
         }
     }
 }
