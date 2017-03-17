a04c4e7c279edbfe8bb1c98c9443c8d60fa75e55
Simon Cropp <simon.cropp@gmail.com>
2017-03-17 13:15:28 +1100

    cleanup

--- src/NServiceBus.WindowsPerformanceCounters/Counters/PerformanceCounterHelper.cs
+++ src/NServiceBus.WindowsPerformanceCounters/Counters/PerformanceCounterHelper.cs
@@ -10,13 +10,6 @@ namespace NServiceBus
         {
             PerformanceCounter counter;
 
-            TryToInstantiatePerformanceCounter(counterName, instanceName, out counter);
-
-            return new PerformanceCounterInstance(counter);
-        }
-
-        private static bool TryToInstantiatePerformanceCounter(string counterName, string instanceName, out PerformanceCounter counter)
-        {
             if (instanceName.Length > 128)
             {
                 throw new Exception($"The endpoint name ('{instanceName}') is too long (longer then {(int) sbyte.MaxValue}) to register as a performance counter instance name. Reduce the endpoint name.");
@@ -32,7 +25,8 @@ namespace NServiceBus
                 throw new Exception(message, exception);
             }
             logger.DebugFormat("'{0}' counter initialized for '{1}'", counterName, instanceName);
-            return true;
+
+            return new PerformanceCounterInstance(counter);
         }
 
         static ILog logger = LogManager.GetLogger(typeof(PerformanceCounterHelper));
