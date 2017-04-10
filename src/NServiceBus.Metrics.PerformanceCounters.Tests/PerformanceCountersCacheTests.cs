82ae11b69ee0ed5f82cfef38dd850ede2d7c65a7
danielmarbach <daniel.marbach@openplace.net>
2017-04-10 11:35:56 +0200

    Added performance counter cache

---
+++ src/NServiceBus.Metrics.PerformanceCounters.Tests/PerformanceCountersCacheTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests
+{
+    using NServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PerformanceCountersCacheTests
+    {
+        [Test]
+        public void Get_the_same_counter_twice_returns_the_same()
+        {
+            var cache = new TestablePerformanceCountersCache();
+
+            var counterName = "CounterName";
+            var instanceName = "InstancenName";
+
+            var firstCounter = cache.Get(new CounterInstanceName(counterName, instanceName));
+            var secondCounter = cache.Get(new CounterInstanceName(counterName, instanceName));
+
+            Assert.NotNull(firstCounter);
+            Assert.NotNull(secondCounter);
+            Assert.AreSame(firstCounter, secondCounter);
+            Assert.AreEqual(1, cache.CountersCreated);
+        }
+
+        class TestablePerformanceCountersCache : PerformanceCountersCache
+        {
+            public int CountersCreated { get; private set; }
+
+            protected override IPerformanceCounterInstance CreateInstance(CounterInstanceName counterInstanceName)
+            {
+                CountersCreated++;
+                return new MockIPerformanceCounter();
+            }
+        }
+    }
+}
