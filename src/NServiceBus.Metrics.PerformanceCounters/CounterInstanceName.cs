b97f7148d1bb2802001909765c38c7263e92a1ff
danielmarbach <daniel.marbach@openplace.net>
2017-04-10 11:35:27 +0200

    Meter and CounterInstanceName structs

---
+++ src/NServiceBus.Metrics.PerformanceCounters/CounterInstanceName.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus
+{
+    using System;
+
+    struct CounterInstanceName : IEquatable<CounterInstanceName>
+    {
+        public bool Equals(CounterInstanceName other)
+        {
+            return string.Equals(CounterName, other.CounterName, StringComparison.InvariantCultureIgnoreCase) && string.Equals(InstanceName, other.InstanceName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is CounterInstanceName && Equals((CounterInstanceName) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (StringComparer.InvariantCultureIgnoreCase.GetHashCode(CounterName) * 397) ^ StringComparer.InvariantCultureIgnoreCase.GetHashCode(InstanceName);
+            }
+        }
+
+        public readonly string CounterName;
+        public readonly string InstanceName;
+
+        public CounterInstanceName(string counterName, string instanceName)
+        {
+            Guard.AgainstNull(nameof(counterName), counterName);
+            Guard.AgainstNull(nameof(instanceName), instanceName);
+
+            CounterName = counterName;
+            InstanceName = instanceName;
+        }
+    }
+}
---
+++ src/NServiceBus.Metrics.PerformanceCounters/Meter.cs
@@ -0,0 +1,14 @@
+﻿namespace NServiceBus
+{
+    struct Meter
+    {
+        public readonly string Name;
+        public readonly long Count;
+
+        public Meter(string name, long count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+}
