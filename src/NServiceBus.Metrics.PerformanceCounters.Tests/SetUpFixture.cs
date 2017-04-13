46b82a801d7474a4ceb74eb8f4a7640661272bdd
danielmarbach <daniel.marbach@openplace.net>
2017-04-13 14:07:36 +0200

    Using generated code for unit tests

--- src/NServiceBus.Metrics.PerformanceCounters.Tests/SetUpFixture.cs
+++ src/NServiceBus.Metrics.PerformanceCounters.Tests/SetUpFixture.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Security;
 using NUnit.Framework;
 
 [SetUpFixture]
@@ -10,31 +6,6 @@ public class SetUpFixture
     [OneTimeSetUp]
     public void SetUp()
     {
-        var counterCreationCollection = new CounterCreationDataCollection(Counters.ToArray());
-        try
-        {
-            if (!PerformanceCounterCategory.Exists("NServiceBus"))
-            {
-                PerformanceCounterCategory.Create(
-                    categoryName: "NServiceBus",
-                    categoryHelp: "NServiceBus statistics",
-                    categoryType: PerformanceCounterCategoryType.MultiInstance,
-                    counterData: counterCreationCollection);
-                PerformanceCounter.CloseSharedResources();
-            }
-        }
-        catch(Exception ex) when(ex is SecurityException || ex is UnauthorizedAccessException)
-        {
-            throw new Exception("Run VS as admin", ex);
-        }
+        CounterCreator.Create();
     }
-
-    static List<CounterCreationData> Counters = new List<CounterCreationData>
-    {
-        new CounterCreationData(PerformanceCountersFeature.CriticalTimeCounterName, "Age of the oldest message in the queue.", PerformanceCounterType.NumberOfItems32),
-        new CounterCreationData(SLAMonitoringFeature.CounterName, "Seconds until the SLA for this endpoint is breached.", PerformanceCounterType.NumberOfItems32),
-        new CounterCreationData(PerformanceCountersFeature.MessagesProcessedPerSecondCounterName, "The current number of messages processed successfully by the transport per second.", PerformanceCounterType.RateOfCountsPerSecond32),
-        new CounterCreationData(PerformanceCountersFeature.MessagesPulledPerSecondCounterName, "The current number of messages pulled from the input queue by the transport per second.", PerformanceCounterType.RateOfCountsPerSecond32),
-        new CounterCreationData(PerformanceCountersFeature.MessagesFailuresPerSecondCounterName, "The current number of failed processed messages by the transport per second.", PerformanceCounterType.RateOfCountsPerSecond32)
-    };
 }
