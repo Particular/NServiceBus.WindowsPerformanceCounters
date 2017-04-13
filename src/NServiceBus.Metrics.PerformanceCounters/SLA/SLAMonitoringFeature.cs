bbeaf28477984340d77be706e6ad75f8342566f4
danielmarbach <daniel.marbach@openplace.net>
2017-04-13 14:37:26 +0200

    SLAMonitoringFeature uses counter cache

--- src/NServiceBus.Metrics.PerformanceCounters/SLA/SLAMonitoringFeature.cs
+++ src/NServiceBus.Metrics.PerformanceCounters/SLA/SLAMonitoringFeature.cs
@@ -1,12 +1,11 @@
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Features;
+using NServiceBus.Metrics.PerformanceCounters.Counters;
 
 class SLAMonitoringFeature : Feature
 {
-   public  const string CounterName = "SLA violation countdown";
     protected override void Setup(FeatureConfigurationContext context)
     {
         context.ThrowIfSendonly();
@@ -14,57 +13,54 @@ class SLAMonitoringFeature : Feature
         var endpointSla = settings.Get<TimeSpan>(EndpointSLAKey);
 
         var counterInstanceName = settings.EndpointName();
-        PerformanceCounter counter1;
 
-        if (counterInstanceName.Length > sbyte.MaxValue)
-        {
-            throw new Exception($"The endpoint name ('{counterInstanceName}') is too long (longer then {sbyte.MaxValue}) to register as a performance counter instance name. Reduce the endpoint name.");
-        }
+        cache = new PerformanceCountersCache();
+        var counter = cache.Get(new CounterInstanceName(CounterName, counterInstanceName));
 
-        try
-        {
-            counter1 = new PerformanceCounter("NServiceBus", CounterName, counterInstanceName, false);
-        }
-        catch (Exception exception)
-        {
-            var message = $"NServiceBus performance counter for '{CounterName}' is not set up correctly. To rectify this problem, consult the NServiceBus performance counters documentation.";
-            throw new Exception(message, exception);
-        }
-        var counter = new PerformanceCounterInstance(counter1);
-        var slaBreachCounter = new EstimatedTimeToSLABreachCounter(endpointSla, counter);
-        var startup = new StartupTask(slaBreachCounter);
+        slaBreachCounter = new EstimatedTimeToSLABreachCounter(endpointSla, counter);
 
         context.Pipeline.OnReceivePipelineCompleted(pipelineCompleted =>
         {
-            slaBreachCounter.Update(pipelineCompleted);
+            slaBreachCounter?.Update(pipelineCompleted);
             return Task.FromResult(0);
         });
 
-        context.RegisterStartupTask(() => startup);
+        context.RegisterStartupTask(new StartupTask(this));
     }
+
+    PerformanceCountersCache cache;
+    EstimatedTimeToSLABreachCounter slaBreachCounter;
+
+    public const string CounterName = "SLA violation countdown";
 
 
     internal const string EndpointSLAKey = "EndpointSLA";
 
-    class StartupTask : FeatureStartupTask
+    class StartupTask : FeatureStartupTask, IDisposable
     {
-        public StartupTask(EstimatedTimeToSLABreachCounter slaBreachCounter)
+        public StartupTask(SLAMonitoringFeature feature)
         {
-            this.slaBreachCounter = slaBreachCounter;
+            this.feature = feature;
+        }
+
+        public void Dispose()
+        {
+            feature.slaBreachCounter?.Dispose();
+            feature.slaBreachCounter = null;
+            feature.cache?.Dispose();
         }
 
         protected override Task OnStart(IMessageSession session)
         {
-            slaBreachCounter.Start();
-            return Task.FromResult(0);
+            feature.slaBreachCounter.Start();
+            return TaskExtensions.CompletedTask;
         }
 
         protected override Task OnStop(IMessageSession session)
         {
-            slaBreachCounter.Dispose();
-            return Task.FromResult(0);
+            return TaskExtensions.CompletedTask;
         }
 
-        EstimatedTimeToSLABreachCounter slaBreachCounter;
+        SLAMonitoringFeature feature;
     }
 }
