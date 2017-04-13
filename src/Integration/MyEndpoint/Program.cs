3a82953448fc8121b5b879f86f04482099210eb7
danielmarbach <daniel.marbach@openplace.net>
2017-04-13 09:42:14 +0200

    Integration solution

---
+++ src/Integration/MyEndpoint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+namespace MyEndpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            var configuration = new EndpointConfiguration("MyEndpoint");
+            await Endpoint.Start(configuration);
+        }
+    }
+}
