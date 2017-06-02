9c1909a1d5165be480c4900203bd6d3325924ab4
Dennis van der Stelt <dvdstelt@outlook.com>
2017-06-02 13:39:15 +0200

    Removed debug statements

--- src/NServiceBus.Metrics.PerformanceCounters/PerformanceCounterUpdater.cs
+++ src/NServiceBus.Metrics.PerformanceCounters/PerformanceCounterUpdater.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json.Linq;
 
 class PerformanceCounterUpdater
@@ -29,15 +28,10 @@ class PerformanceCounterUpdater
         var timers = rootObject["Timers"]?.ToObject<List<Timer>>() ?? new List<Timer>();
         foreach (var timer in timers)
         {
-            //Console.WriteLine($"{timer.Name} - {timer.Histogram.LastValue}");
-            //Console.WriteLine($"{timer.Name} - {Math.Round((double)timer.Histogram.LastValue / 1000)}");
-
             var counterInstanceName = new CounterInstanceName(timer.Name, context);
             var performanceCounterInstance = cache.Get(counterInstanceName);
-
-            Console.WriteLine($"{timer.Name} - \n\tOld: {performanceCounterInstance.RawValue} / New: {timer.Histogram.LastValue}");
-
-            performanceCounterInstance.RawValue = timer.Histogram.LastValue;
+            
+            performanceCounterInstance.RawValue = timer.Histogram.LastValue / 1000;
         }
     }
 
