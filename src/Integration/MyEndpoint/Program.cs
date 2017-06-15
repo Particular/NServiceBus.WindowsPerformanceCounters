f434ee782ae1bcc1934134d82d883e43265e9903
Simon Cropp <simon.cropp@gmail.com>
2017-06-15 12:02:46 +1000

    split files

--- src/Integration/MyEndpoint/Program.cs
+++ src/Integration/MyEndpoint/Program.cs
@@ -2,46 +2,32 @@
 using System.Threading.Tasks;
 using NServiceBus;
 
-namespace MyEndpoint
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            AsyncMain().GetAwaiter().GetResult();
-        }
-
-        static async Task AsyncMain()
-        {
-            var configuration = new EndpointConfiguration("MyEndpoint");
-            configuration.SendFailedMessagesTo("error");
-            configuration.UsePersistence<InMemoryPersistence>();
-            configuration.UseTransport<MsmqTransport>();
-            configuration.EnableInstallers();
-            var performanceCounters = configuration.EnableWindowsPerformanceCounters();
-            performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(1));
-            var endpoint = await Endpoint.Start(configuration);
-
-            ConsoleKeyInfo readKey;
-            do
-            {
-                await endpoint.SendLocal(new MyMessage());
-
-                readKey = Console.ReadKey();
-            } while (readKey.Key != ConsoleKey.Escape);
-
-            await endpoint.Stop();
-        }
+        AsyncMain().GetAwaiter().GetResult();
     }
 
-    public class MyMessage : ICommand
+    static async Task AsyncMain()
     {
-    }
+        var configuration = new EndpointConfiguration("MyEndpoint");
+        configuration.SendFailedMessagesTo("error");
+        configuration.UsePersistence<InMemoryPersistence>();
+        configuration.UseTransport<MsmqTransport>();
+        configuration.EnableInstallers();
+        var performanceCounters = configuration.EnableWindowsPerformanceCounters();
+        performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(1));
+        var endpoint = await Endpoint.Start(configuration).ConfigureAwait(false);
 
-    public class MyHandler : IHandleMessages<MyMessage> {
-        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        ConsoleKeyInfo readKey;
+        do
         {
-            return Task.Delay(2000);
-        }
+            await endpoint.SendLocal(new MyMessage()).ConfigureAwait(false);
+
+            readKey = Console.ReadKey();
+        } while (readKey.Key != ConsoleKey.Escape);
+
+        await endpoint.Stop().ConfigureAwait(false);
     }
 }
