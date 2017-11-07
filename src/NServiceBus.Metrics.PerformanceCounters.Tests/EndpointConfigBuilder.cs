78f04c2ec12ab4235182f890c5bce0ed468a8289
Szymon Kulec <scooletz@gmail.com>
2017-11-07 10:32:04 +0100

    Move tests to the newest core v6 to reintroduce the learning transport

--- src/NServiceBus.Metrics.PerformanceCounters.Tests/EndpointConfigBuilder.cs
+++ src/NServiceBus.Metrics.PerformanceCounters.Tests/EndpointConfigBuilder.cs
@@ -5,7 +5,7 @@ static class EndpointConfigBuilder
     public static EndpointConfiguration BuildEndpoint(string s)
     {
         var endpointConfiguration = new EndpointConfiguration(s);
-        endpointConfiguration.UseTransport<MsmqTransport>();
+        endpointConfiguration.UseTransport<LearningTransport>();
         endpointConfiguration.SendFailedMessagesTo("error");
         endpointConfiguration.EnableInstallers();
         endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
