412e906be9336cfd4d63304f5625c37ac9077383
David Boike <david.boike@gmail.com>
2019-07-16 13:10:11 -0500

    NetCore support (#71)
    * NetCore support
    
    * Add netcoreapp2.1 dependencies to the tests project
    
    * Magic parameter
    
    * Adjust ApiApproval for multi-framework
    
    * Throw on non-Windows before endpoint even starts
    
    * Inspections
    
    * Restoring original file permissions
    
    * Direct references, drop 2_0
    
    * Reorder ItemGroups to fit conventions

--- src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersExtensions.cs
+++ src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersExtensions.cs
@@ -1,6 +1,5 @@
 namespace NServiceBus
 {
-
     /// <summary>
     /// Exposes windows performance counters configuration on <see cref="EndpointConfiguration"/>.
     /// </summary>
@@ -12,6 +11,12 @@ namespace NServiceBus
         /// <param name="endpointConfiguration">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
         public static PerformanceCountersSettings EnableWindowsPerformanceCounters(this EndpointConfiguration endpointConfiguration)
         {
+#if NETSTANDARD
+            if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                throw new System.PlatformNotSupportedException("Windows Performance Counters are not supported on this platform.");
+            }
+#endif
             Guard.AgainstNull(nameof(endpointConfiguration), endpointConfiguration);
 
             endpointConfiguration.EnableFeature<PerformanceCountersFeature>();
--- src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersSettings.cs
+++ src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersSettings.cs
@@ -21,6 +21,12 @@
         /// <param name="sla">The SLA to use. Must be greater than <see cref="TimeSpan.Zero" />.</param>
         public void EnableSLAPerformanceCounters(TimeSpan sla)
         {
+#if NETSTANDARD
+            if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                throw new PlatformNotSupportedException("Windows Performance Counters are not supported on this platform.");
+            }
+#endif
             Guard.AgainstNegativeAndZero(nameof(sla), sla);
 
             endpointConfiguration.GetSettings().Set(SLAMonitoringFeature.EndpointSLAKey, sla);
