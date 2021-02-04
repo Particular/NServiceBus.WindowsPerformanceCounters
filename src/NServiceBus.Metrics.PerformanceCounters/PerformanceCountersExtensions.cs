349fcbfc590f0ebd6a4626c5885a5e61028a5610
Yves Goeleven <yves@goeleven.com>
2021-02-04 10:47:55 +0100

    target framework upgrade

--- src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersExtensions.cs
+++ src/NServiceBus.Metrics.PerformanceCounters/PerformanceCountersExtensions.cs
@@ -11,12 +11,11 @@ namespace NServiceBus
         /// <param name="endpointConfiguration">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
         public static PerformanceCountersSettings EnableWindowsPerformanceCounters(this EndpointConfiguration endpointConfiguration)
         {
-#if NETSTANDARD
             if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
             {
                 throw new System.PlatformNotSupportedException("Windows Performance Counters are not supported on this platform.");
             }
-#endif
+
             Guard.AgainstNull(nameof(endpointConfiguration), endpointConfiguration);
 
             endpointConfiguration.EnableMetrics();
